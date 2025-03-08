5bebae717c0cb7e3a6daf16132a1d4c89c4c9b91
Rootcreator <maarrxxy@gmail.com>
2025-03-08 12:07:41 +0100

    v1.2

--- investment/settings.py
+++ investment/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Directory where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
